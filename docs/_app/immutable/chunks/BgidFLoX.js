7cd7402391340e606cdb1de7ff0d287977542f04
Alfred Berggren <alfber10@edu.umea.se>
2026-02-06 12:57:44 +0100

    message

---
+++ docs/_app/immutable/chunks/BgidFLoX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1weqwqa?.base??"",a=globalThis.__sveltekit_1weqwqa?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/Z2AmGlhW.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jrard?.base??"",a=globalThis.__sveltekit_jrard?.assets??s??"";export{a,s as b};
